37e5e615e8b118039ec5a066120397c7788e6694
Fahad_2022 <rjfahad44@gmail.com>
2023-05-30 22:02:35 +0600

    update

---
+++ app/src/main/java/com/ft/compose_search_widget_in_app_bar/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.ft.compose_search_widget_in_app_bar
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+
+
+}
