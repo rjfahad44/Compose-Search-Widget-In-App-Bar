fcb55a08e14ecc6c969c4c7603c611bac727920e
Fahad_2022 <rjfahad44@gmail.com>
2023-05-30 21:54:16 +0600

    update

---
+++ app/src/main/java/com/ft/compose_search_widget_in_app_bar/MainScreen.kt
@@ -0,0 +1,25 @@
+package com.ft.compose_search_widget_in_app_bar
+
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MainScreen(mainViewModel: MainViewModel) {
+
+}
